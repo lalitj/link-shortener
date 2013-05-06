822237171d042c7d22ec57f3c7e0d67adda5727b
unknown <Lalit@lucky-pc.(none)>
2013-05-06 12:08:04 +0530

    db schema

--- app/models/Urls.php
+++ app/models/Urls.php
@@ -3,7 +3,17 @@
 namespace app\models;
 
 class Urls extends \lithium\data\Model {
-
+	
+	//Ref : http://blog.marcghorayeb.com/post/13013812393/mongodb-schema-definitions-in-li3
+	protected $_schema = array(
+		'_id' => array('type' => 'id'),
+		'url' => array('type' => 'string'),
+		'hash' => array('type' => 'string'),
+		'shortlink' => array('type' => 'string'),
+		'date' => array('type' => 'timestamp'), 
+		'views' => array('type' => 'integer')
+	);
+	
 	public $validates = array();
 }
 
